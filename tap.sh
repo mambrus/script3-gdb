d452449fa1232d2f99a1961acb3bc9bc0c673c21
Michael Ambrus <michael.ambrus@sonyericsson.com>
2011-12-13 16:02:12 +0100

    Round-up commit
    I.e. various unrelated commits

--- tap.sh
+++ tap.sh
@@ -2,10 +2,19 @@
 # Author: Michael Ambrus (ambrmi09@gmail.com)
 # 2011-03-05
 # Shell wraps tcp_tap to make it gdb.tap.sh
+# Use this file "as-is" or as a template for your own wrappers. Mandatory
+# adjustments are TCP_TAP_EXEC & TCP_TAP_PORT, which need to be unique for each
+# program you intend to extend with a "tap".
+
 
 if [ -z $TAP_SH ]; then
 
-TAP_SH="tap.sh"
+TAP_SH="gdb.tap.sh"
+FIRST_PORT="8080"
+XTERM_DELAY="1"
+
+#Number of other sessions allredy in use (collision avoidande)
+NR_INUSE=$(ps -Al | grep tcp_tap | wc -l)
 
 # This script works as the basename command, except that it also
 # ripps away everything in the name before the next but last '.'
@@ -13,23 +22,22 @@ TAP_SH="tap.sh"
 # $ tap /some/path/pre.fix.myshell.sh
 #   myshell.sh
 #
-# The script is a core part of the 'script3' script library
 
 function tap() {
 	exec tcp_tap "$@"
 }
 
-source s3.ebasename.sh
-if [ "$TAP_SH" == $( ebasename $0 ) ]; then
+if [ "$TAP_SH" == $( basename $0 ) ]; then
 	#Not sourced, do something with this.
 
-	export TCP_TAP_EXEC="/skiff/bin/arm-hixs-elf-gdb"
-	export TCP_TAP_PORT="8088"
-	#export TCP_TAP_LOG_STDIN="/dev/null"
-	#export TCP_TAP_LOG_STDOUT="/dev/null"
-	#export TCP_TAP_LOG_STDERR="/dev/null"
-	#export TCP_TAP_LOG_PARENT="/dev/null"
-	#export TCP_TAP_LOG_CHILD="/dev/null"
+	export TCP_TAP_EXEC="$(which gdb_arm)"
+	export TCP_TAP_PORT=$(( FIRST_PORT + NR_INUSE ))
+	gdb.telnet.sh localhost $TCP_TAP_PORT $XTERM_DELAY &
+	export TCP_TAP_LOG_STDIN="/dev/null"
+	export TCP_TAP_LOG_STDOUT="/dev/null"
+	export TCP_TAP_LOG_STDERR="/dev/null"
+	export TCP_TAP_LOG_PARENT="/dev/null"
+	export TCP_TAP_LOG_CHILD="/dev/null"
 
 	tap "$@"
 	exit $?
